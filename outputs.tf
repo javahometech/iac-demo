23d1d62bc0d8d7398f0e768b97c065f882397611
Ragesh <Rageshmoorkoth@gmail.com>
2021-04-13 15:33:13 -0400

    deleted-output

--- outputs.tf
+++
@@ -1,6 +0,0 @@
-output "jenkins_master_alb_dns_name" {
-  description = "Jenkins Master Application Load Balancer DNS Name"
-  value       = module.ecs-fargate-service.aws_lb_lb_dns_name
-}
-/// comments added///
-//chnaged//
