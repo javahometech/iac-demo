928e3904a6ee9f8cb875c51194ba30b745b85665
Ragesh <Rageshmoorkoth@gmail.com>
2021-04-13 15:17:04 -0400

    nameupdated

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "us-east-1"
 }
-resource "aws_instance" "Ragesh-n" {
+resource "aws_instance" "Ragesh-new" {
   ami = "ami-0bcf5425cdc1d8a85"
   instance_type = "t3.micro"
   key_name = "terraform-Jenkins"
