613e5f4b045b23bb9e31f98a8d230e8e9fba4aee
Ragesh <Rageshmoorkoth@gmail.com>
2021-04-14 15:25:41 -0400

    variable defined

--- variables.tf
+++ variables.tf
@@ -2,15 +2,23 @@
 # Misc
 #------------------------------------------------------------------------------
 variable "instance_name" {
-  description = "Name prefix for resources on AWS"
-  type = string
-  default = "test-instance"
+description = "Name prefix for resources on AWS"
+type = string
+default = "test-instance"
 }
 
 #------------------------------------------------------------------------------
 # AWS CREDENTIALS AND REGION
 #------------------------------------------------------------------------------
+variable "instance_type" {
+description = "AWS EC2 instance type"
+default = "t2.micro"
+}
 variable "region" {
-  description = "AWS Region the infrastructure is hosted in"
-  default = "us-east-1"
+description = "AWS Region the infrastructure is hosted in"
+default = "us-east-1"
+}
+variable "cloudprovider" {
+description = "Select the Cloud Provider"
+default = "AWS"
 }
