f2c66a1873b64a8a43852c6d179f0a6c57750600
Ragesh <Rageshmoorkoth@gmail.com>
2021-04-14 16:50:02 -0400

    updated

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "Ragesh" {
 ami = "ami-0bcf5425cdc1d8a85"
 instance_type = var.instance_type
 key_name = "terraform-Jenkins"
-region = var.region
 tags = {
   Name = var.instance_name
    }
