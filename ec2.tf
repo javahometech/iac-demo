a5c037e5cc9839e6a3c52b8721394e695dbe766c
Ragesh <Rageshmoorkoth@gmail.com>
2021-05-01 10:32:22 -0400

    updated size variable

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "Ragesh" {
   ami = "ami-045e6fa7127ab1ac4"
 instance_type = var.instance_type
 key_name = "terraform-Jenkins"
-size == var.size
+volume_size = var.size
+
 tags = {
   Name = var.instance_name
   Owner = var.Owner
