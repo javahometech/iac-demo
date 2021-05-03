7d2add36a8edee5c52ec1871938af698b8e80b50
Ragesh <Rageshmoorkoth@gmail.com>
2021-05-03 15:56:09 -0400

    volume-size-removed

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,6 @@ resource "aws_instance" "Ragesh" {
   ami = "ami-045e6fa7127ab1ac4"
 instance_type = var.instance_type
 key_name = "terraform-Jenkins"
-volume_size = var.size
-
 tags = {
   Name = var.instance_name
   Owner = var.Owner
