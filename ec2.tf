c7348b55fda6b4c28297ea05803ae3114a0f21d3
Ragesh <Rageshmoorkoth@gmail.com>
2021-05-10 11:37:31 -0400

    ansible-updated

--- ec2.tf
+++ ec2.tf
@@ -6,9 +6,6 @@ tags = {
   Name = var.instance_name
   Owner = var.Owner
    }
-   provisioner "local-exec" {
-    command = "ansible-playbook -u ec2-user -i '${aws_instance.Ragesh.*.public_ip}', --private-key POC-STD-KEY-PAIR.pem playbook.yml >>/tmp/ansible-output"
   }
-}
 
 
