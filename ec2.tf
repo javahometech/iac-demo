6822310bb1d5ce73f573615cf1f79de52f4bbfd2
Ragesh <Rageshmoorkoth@gmail.com>
2021-05-10 00:04:53 -0400

    key-pair-name-updated

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Ragesh" {
   ami = "ami-045e6fa7127ab1ac4"
 instance_type = var.instance_type
-key_name = "terraform-Jenkins"
+key_name = "POC-STD-KEY-PAIR"
 tags = {
   Name = var.instance_name
   Owner = var.Owner
