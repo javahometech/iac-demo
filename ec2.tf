517fea3a35f5aa0cbfcea534e2e933773ccf5885
Ragesh <Rageshmoorkoth@gmail.com>
2021-05-10 16:47:21 -0400

    inventory-file-created

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,19 @@ tags = {
   Name = var.instance_name
   Owner = var.Owner
    }
+    provisioner "local-exec" {
+    command = "ansible-playbook -u ec2-user -i inventory  --private-key POC-STD-KEY-PAIR.pem playbook.yml"
+  }
   }
 
+
+resource "local_file" "ansible_inventory_hosts" {
+ content = templatefile("inventory.template",
+ {
+  web_public_ip = ${aws_instance.Ragesh.*.public_ip},
+
+ }
+ )
+ filename = "inventory"
+}
 
