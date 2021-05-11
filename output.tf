6141336a0e3377c265335b0052b31a459a7894c2
Ragesh <Rageshmoorkoth@gmail.com>
2021-05-11 03:51:10 -0400

    upated outputfile

--- output.tf
+++ output.tf
@@ -1,10 +1,11 @@
-output "ip" {
+output "AWS-instance-Public-IP" {
   value = "${aws_instance.Ragesh.public_ip}"
 }
 resource "local_file" "ansible_inventory_hosts" {
  content = templatefile("inventory.template",
  {
   web_public_ip = aws_instance.Ragesh.public_ip,
+  web_id = aws_instance.Ragesh.id,
  }
  )
  filename = "inventory"
