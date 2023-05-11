07d8398f952991309cd9ffe573153e95ca633955
praveenchary <52570158+praveenchary17@users.noreply.github.com>
2023-05-11 15:53:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-007855ac798b5175e"
-  instance_type = "t3.micro"
+  instance_type = "t2.small"
 
   tags = {
     Name = "github-jenkins-terraform"
