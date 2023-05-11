586cf95ff4c898b13e6f8c66f3e634514fb44b41
praveenchary <52570158+praveenchary17@users.noreply.github.com>
2023-05-11 14:49:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAZHO3C2CJO6FI4AWR"
-  secret_key = "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"
+  access_key = "AKIAZHO3C2CJH5DPHUVO"
+  secret_key = "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"
 }
 
